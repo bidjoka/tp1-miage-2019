ae8fe91823497e403b6ecf2299088ce6c8f6576e
HP <HP@DESKTOP-3ODD51A>
2019-04-14 19:10:31 +0200

    test cumcumber

--- ex5-ini/mailreader/src/test/java/com/acme/mailreader/bdd/MailComparaisonStep.java
+++ ex5-ini/mailreader/src/test/java/com/acme/mailreader/bdd/MailComparaisonStep.java
@@ -3,12 +3,12 @@ package com.acme.mailreader.bdd;
 import static org.junit.Assert.assertThat;
 
 import static org.hamcrest.core.Is.is;
-import java.time.Instant;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
 import com.acme.mailreader.domain.DateIncorrecteException;
+import com.acme.mailreader.domain.InstantConverter;
 import com.acme.mailreader.domain.Mail;
 import com.acme.mailreader.domain.Mail.Statut;
 import com.acme.mailreader.domain.MailComparator;
@@ -30,6 +30,7 @@ public class MailComparaisonStep {
 
 	private Mail mail1;
 	private Mail mail2;
+	private InstantConverter inst;
 	private String resultatComparaison;
 	Comparator<Mail> comparator = new MailComparator();
 	private static final Map<Integer, String> resuAsString = new HashMap<Integer, String>();
@@ -43,13 +44,13 @@ public class MailComparaisonStep {
 	@Given("^un premier mail avec l'importance \"([^\"]*)\", le statut \"([^\"]*)\", le sujet \"([^\"]*)\" et la date \"([^\"]*)\"$")
 	public void un_premier_mail(boolean importance, Statut statut,
 			String sujet, String date) throws DateIncorrecteException {
-		mail1 = new Mail.Builder(sujet).important(importance).statut(statut).date(Instant.parse(date)).build();
+		mail1 = new Mail.Builder(sujet).important(importance).statut(statut).date(inst.transform(date)).build();
 	}
 
 	@Given("^un second mail avec l'importance \"([^\"]*)\", le statut \"([^\"]*)\", le sujet \"([^\"]*)\" et la date \"([^\"]*)\"$")
 	public void un_second_mail(boolean importance, Statut statut, String sujet,
 			String date) throws DateIncorrecteException {
-		mail2 = new Mail.Builder(sujet).important(importance).statut(statut).date(Instant.parse(date)).build();
+		mail2 = new Mail.Builder(sujet).important(importance).statut(statut).date(inst.transform(date)).build();
 	}
 
 	@When("^je trie$")
